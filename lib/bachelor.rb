879af9b233fc96356e8aa8cc0ba80e3cc4e304c4
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-04-02 21:30:05 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,17 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_number, contestant_array|
+  if season_number == season
+  contestant_array.each do |detail_hash|
+    detail_hash.each do |key, value|
+  if value == "Winner" 
+    return detail_hash["name"].split(" ")[0]
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
